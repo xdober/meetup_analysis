0202a2eff7aa696f05e3ecd4d135c0c51c6e1132
xdober <xuduo1017@live.com>
2018-01-21 16:57:42 +0800

    groups number per year

--- read.py
+++ read.py
@@ -46,40 +46,48 @@ group_attrs = group_info.columns
 # group_rating=group_info['group_id'].groupby(pd.cut(group_info.rating, ranges)).count()
 # group_rating.plot.bar()
 # plt.show()
-# groups per city
-group_split_city = info_split(group_info,'city')
-fig1 = plt.figure()
-axes1 = fig1.add_axes([0.1, 0.1, 0.8, 0.8])
-axes1.bar(list(arange(len(group_split_city)) + 1), group_split_city)
-axes1.xaxis.set_visible(False)
-axes1.set_title('groups number per city')
-# group_split_city.plot.bar()
-for x, y, z in zip(np.arange(len(group_split_city)), group_split_city, group_split_city._index):
-    plt.text(x + 1, y + 80, '%d' % y, ha='center', va='center')
-    plt.text(x + 1, -400, z, ha='center', va='center', rotation=15)
+# # groups per city
+# group_split_city = info_split(group_info,'city')
+# fig1 = plt.figure()
+# axes1 = fig1.add_axes([0.1, 0.1, 0.8, 0.8])
+# axes1.bar(list(arange(len(group_split_city)) + 1), group_split_city)
+# axes1.xaxis.set_visible(False)
+# axes1.set_title('groups number per city')
+# for x, y, z in zip(np.arange(len(group_split_city)), group_split_city, group_split_city._index):
+#     plt.text(x + 1, y + 80, '%d' % y, ha='center', va='center')
+#     plt.text(x + 1, -400, z, ha='center', va='center', rotation=15)
+#
+# # groups per category
+# group_split_category = info_split(group_info, 'category.shortname')
+# fig2 = plt.figure()
+# axes2 = fig2.add_axes(([0.1, 0.2, 0.8, 0.7]))
+# axes2.bar(arange(len(group_split_category)) + 1, group_split_category)
+# axes2.xaxis.set_visible(False)
+# axes2.set_title('groups number per category')
+# for x, y, z in zip(np.arange(len(group_split_category)), group_split_category, group_split_category._index):
+#     plt.text(x + 1, y + 80, '%d' % y, ha='center', va='center')
+#     plt.text(x + 1, -400, z, ha='center', va='center', rotation=60)
+#
+# # groupby rating
+# ranges = np.arange(0,5.5,0.5)
+# group_rating=group_info['group_id'].groupby(pd.cut(group_info.rating, ranges)).count()
+# fig3=plt.figure()
+# axes3=fig3.add_axes([0.1,0.2,0.8,0.7])
+# axes3.set_title('groups number per rating')
+# for x,y in zip(np.arange(len(group_rating)), group_rating):
+#     plt.text(x, y+200, '%d' % y, ha='center', va='center')
+# group_rating.plot.bar()
 
-# groups per category
-group_split_category = info_split(group_info, 'category.shortname')
-fig2 = plt.figure()
-axes2 = fig2.add_axes(([0.1, 0.2, 0.8, 0.7]))
-axes2.bar(arange(len(group_split_category)) + 1, group_split_category)
-axes2.xaxis.set_visible(False)
-axes2.set_title('groups number per category')
-for x, y, z in zip(np.arange(len(group_split_category)), group_split_category, group_split_category._index):
-    plt.text(x + 1, y + 80, '%d' % y, ha='center', va='center')
-    plt.text(x + 1, -400, z, ha='center', va='center', rotation=60)
-
-# groupby rating
-ranges = np.arange(0,5.5,0.5)
-group_rating=group_info['group_id'].groupby(pd.cut(group_info.rating, ranges)).count()
-fig3=plt.figure()
-axes3=fig3.add_axes([0.1,0.2,0.8,0.7])
-axes3.set_title('groups number per rating')
-for x,y in zip(np.arange(len(group_rating)), group_rating):
-    plt.text(x, y+200, '%d' % y, ha='center', va='center')
-# axes3.bar(group_rating._index,group_rating)
-group_rating.plot.bar()
+# groupby created date
+group_created=group_info['group_id'].groupby(pd.to_datetime(group_info.created).dt.year).count()
+fig4=plt.figure()
+axes4=fig4.add_axes([0.1,0.1,0.8,0.8])
+axes4.set_title('groups number per year')
+group_created.plot.bar()
+for x,y in zip(np.arange(len(group_created)), group_created):
+    plt.text(x, y+100, '%d' % y, ha='center', va='center')
 plt.show()
+
 # group_split_category=group_info['category.shortname'].value_counts()
 # print(group_split_category)
 # group_split_category.plot.bar()
