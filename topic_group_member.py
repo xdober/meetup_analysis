dea1990d36433f9e94e8c2ebf17b41826b58f270
xdober <xuduo1017@live.com>
2018-04-01 12:01:01 +0800

    topic group

--- topic_group_member.py
+++ topic_group_member.py
@@ -6,5 +6,9 @@ from pylab import *
 group_topic_info=pd.read_csv(Const.GRP_TPC_PATH, encoding="iso-8859-1")
 group=pd.read_csv(Const.GROUP_PATH)
 group_topic=pd.merge(group_topic_info,group,on='group_id')[['topic_id','topic_key','topic_name','group_id','category_id','category.shortname']]
-grouped=group_topic.groupby(['topic_id','topic_name','category_id','category.shortname'])
-print(grouped.size())
+grouped_topic_group=group_topic.groupby(['topic_name','category.shortname'])
+grouped_topic=group_topic.groupby('topic_name')
+# print(grouped_topic_group.size())
+print(grouped_topic.count()\
+      .reset_index()
+      .sort_values('group_id',ascending=False))
