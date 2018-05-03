ae654b4f70a53de66dcde2708d9a8b4ad7c97784
xdober <xuduo1017@live.com>
2018-05-03 12:24:43 +0800

    member groups trends

--- DynamicEvent.py
+++ DynamicEvent.py
@@ -11,12 +11,40 @@ def event_per_group(event_info):
     events_per_group.sort(key=len,reverse=True)
     print(events_per_group[30])
     return events_per_group
+
+# 统计在某个时间点之前的数量
+def beforeTime(df,tm,created):
+    delta=pd.to_datetime('20110201')-pd.to_datetime('20110101')
+    ndf=df[df[created]<tm]
+    ndf=ndf[ndf[created]>tm-delta]
+    return len(ndf)/31
+# group中member随时间的变化
+# df:group_id相同的member_join_group数据
+def memberTrendsOneGroup(df,created='created'):
+    num_ser=[]
+    start_time=df[created].min()
+    end_time=df[created].max()
+    now=df[created].min()
+    gap=pd.to_datetime('20110102')-pd.to_datetime('20110101')
+    while (now<=end_time):
+        num=beforeTime(df,now,created)
+        num_ser.append(num)
+        now=now+gap
+    num_ser=pd.Series(data=num_ser,index=pd.date_range(start_time,periods=len(num_ser)))
+    return num_ser
+
 # 处理event信息
 def dealEvents():
     event_info=pd.read_csv(Const.EVENT_PATH)
+    event_info['created']=pd.to_datetime(event_info['created'])
+    event_info['event_time']=pd.to_datetime(event_info['event_time'])
     events_per_group = event_per_group(event_info)
-    event_created_fig = rd.info_groupedby_created(events_per_group[0], gap='month', notsave='yes', created='created')
-    event_start_fig = rd.info_groupedby_created(events_per_group[0], gap='month', notsave='yes', created='event_time')
+    # event_created_fig = rd.info_groupedby_created(events_per_group[0], gap='month', notsave='yes', created='created')
+    # event_start_fig = rd.info_groupedby_created(events_per_group[0], gap='month', notsave='yes', created='event_time')
+    ooo=memberTrendsOneGroup(event_info,'event_time')
+    ggg=memberTrendsOneGroup(event_info)
+    plt.plot(ooo)
+    plt.plot(ggg)
     plt.show()
 
 # 简化event信息
